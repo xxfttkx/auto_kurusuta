1278485086ff25a8f9cb138691e6c7853f722589
lauirf <592184299@qq.com>
2025-10-05 22:08:47 +0800

    feat: resize screenshot to 1080x720 for improved template matching

--- task.py
+++ task.py
@@ -20,6 +20,7 @@ class Task:
         while count<times:
             self.controller.activate_target_window()
             screenshot = screenshot_window(self.controller.target_window)
+            screenshot = cv2.resize(screenshot, (1080, 720))
             res = cv2.matchTemplate(screenshot, image, cv2.TM_CCOEFF_NORMED)
             _, max_val, _, max_loc = cv2.minMaxLoc(res)
             x, y = max_loc
