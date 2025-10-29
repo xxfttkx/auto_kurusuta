dd5c67be9c001e1e86aaafe51a549d4852c1d68d
lauirf <592184299@qq.com>
2025-10-29 16:30:59 +0800

    fix: DailyTask may click failed

--- task.py
+++ task.py
@@ -154,9 +154,9 @@ class DailyTask(Task):
         self.match_template_and_click(self.quest_btn, threshold=0.5)
         self.match_template_and_click(self.daily_1_btn, threshold=0.5)
         self.match_template_and_click(self.skip_daily_1, threshold=0.5)
-        self.match_template_and_click(self.ok, threshold=0.5)
+        self.match_template_and_click(self.ok, threshold=0.5, click_delay=0.5)
         time.sleep(1)  # 等待 1 秒，确保界面稳定
-        self.match_template_and_click(self.hai, threshold=0.5)
+        self.match_template_and_click(self.hai, threshold=0.5, click_delay=0.5)
         for _ in range(7):
             time.sleep(1)
             self.controller.click(640, 620)
